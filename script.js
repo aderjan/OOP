601d7d4c7e20e6a6cb6a12d7917c46cdcf00f68a
Adrian Tomaszkiewicz <Adrian Tomaszkiewicz>
2017-02-24 15:24:28 +0100

    First commit

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function Telefon(marka, cena, kolor, ocena) {
-	this.marka = marka; 
-	this.cena = cena;
-	this.kolor = kolor;
-	this.ocena = ocena;
+function Button(text) {
+	this.text = text || 'Cześć!';
 }
-Telefon.prototype.printInfo = function() {
-		console.log('Marka telefonu to ' + this.marka + ', kolor to ' + this.kolor + ', cena to ' + this.cena + ', a ocena to ' + this.ocena + '.');
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.click(function(){
+			alert(self.text);
+		});
+		this.$element.appendTo($('body'));
+	}
 }
-var iPhone6S = new Telefon('Apple', '2250', 'srebrny', '10');
-iPhone6S.printInfo();
-
-var Motorolag3 = new Telefon('Motorola', '800', 'czarny', '6');
-Motorolag3.printInfo();
+var btn1 = new Button('Hello!');
+btn1.create();
